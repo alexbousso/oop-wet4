d2d4017fba3f41eec9730c1a3472d737e9ee8019
Alex Bousso <alexbousso@gmail.com>
2015-06-06 17:07:38 +0300

    Added spaceholders for tests

---
+++ src/cs236703/spring2015/hw4/test/TestingTestMittelman.java
@@ -0,0 +1,14 @@
+package cs236703.spring2015.hw4.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingTestMittelman {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
